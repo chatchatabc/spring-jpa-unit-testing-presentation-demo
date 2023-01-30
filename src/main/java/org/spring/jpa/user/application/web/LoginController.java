d38e7c382fdad785098654d26b97ea5a6736b483
Libing Chen <libing.chen@gmail.com>
2023-01-30 19:09:06 +0800

    chore: add exception catch

--- src/main/java/org/spring/jpa/user/application/web/LoginController.java
+++ src/main/java/org/spring/jpa/user/application/web/LoginController.java
@@ -1,10 +1,11 @@
 package org.spring.jpa.user.application.web;
 
 import org.spring.jpa.user.domain.error.UserNotFoundException;
-import org.spring.jpa.user.domain.service.UserService;
 import org.spring.jpa.user.domain.model.User;
+import org.spring.jpa.user.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -24,11 +25,12 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(String email, String password) throws UserNotFoundException {
-        User result = userService.authUser(email, password);
-        if (result != null) {
+    public String login(String email, String password, Model model) {
+        try {
+            final User user = userService.authUser(email, password);
+            model.addAttribute("user", user);
             return "homepage";
-        } else {
+        } catch (UserNotFoundException e) {
             return "login";
         }
     }
