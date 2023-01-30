8476d6ac45afe616adaf5e5a783dd28b0ecbb7b6
Libing Chen <libing.chen@gmail.com>
2023-01-30 22:48:18 +0800

    test: add @DBUnit

--- src/test/java/org/spring/jpa/user/SpringBaseTest.java
+++ src/test/java/org/spring/jpa/user/SpringBaseTest.java
@@ -1,5 +1,7 @@
 package org.spring.jpa.user;
 
+import com.github.database.rider.core.api.configuration.DBUnit;
+import com.github.database.rider.core.api.configuration.Orthography;
 import com.github.database.rider.spring.api.DBRider;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -7,5 +9,6 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("test")
 @DBRider
+@DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE)
 public class SpringBaseTest {
 }
