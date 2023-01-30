7f3e3e98f6df1acf630757aa816694fb203cdcd6
Libing Chen <libing.chen@gmail.com>
2023-01-30 22:53:24 +0800

    test: add testLoadDataset()

--- src/test/java/org/spring/jpa/user/DataBaseTest.java
+++ src/test/java/org/spring/jpa/user/DataBaseTest.java
@@ -1,8 +1,10 @@
 package org.spring.jpa.user;
 
+import com.github.database.rider.core.api.dataset.DataSet;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatDtdDataSet;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -15,6 +17,12 @@ import java.io.FileOutputStream;
  * @author linux_china
  */
 public class DataBaseTest extends SpringBaseTest {
+
+    @Test
+    @DataSet("db/datasets/users.xml")
+    public void testLoadDataset() {
+        Assertions.assertTrue(true);
+    }
 
     @Test
     public void testDTDGeneration(@Autowired DataSource dataSource) throws Exception {
