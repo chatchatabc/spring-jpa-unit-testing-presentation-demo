db7e312b6b699370ccdadcd3ccdf098b175bdeeb
Libing Chen <libing.chen@gmail.com>
2023-01-30 22:48:03 +0800

    test: polishing

--- src/test/java/org/spring/jpa/user/impl/domain/service/UserServiceImplTest.java
+++ src/test/java/org/spring/jpa/user/impl/domain/service/UserServiceImplTest.java
@@ -1,38 +1,25 @@
-package org.spring.jpa.user.domain.service;
+package org.spring.jpa.user.impl.domain.service;
 
-import com.github.database.rider.core.api.configuration.DBUnit;
-import com.github.database.rider.core.api.configuration.Orthography;
 import com.github.database.rider.core.api.dataset.DataSet;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.spring.jpa.user.SpringBaseTest;
 import org.spring.jpa.user.domain.model.User;
+import org.spring.jpa.user.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
-@DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE)
-public class UserServiceTest extends SpringBaseTest {
+public class UserServiceImplTest extends SpringBaseTest {
 
-    UserService userService;
-    User user;
+    @Autowired
+    private UserServiceImpl userService;
 
     @Mock
     UserService userServiceMock = mock(UserService.class);
-
-    @BeforeEach
-    void createNewInstance() {
-        user = new User();
-    }
-
-    @Autowired
-    public UserServiceTest(UserService userService) {
-        this.userService = userService;
-    }
-
+    
     @Test
     public void sampleTest() {
         String actualVal = "tet123";
@@ -45,21 +32,18 @@ public class UserServiceTest extends SpringBaseTest {
     @Test
     @DataSet("db/datasets/users.xml")
     public void authUserTest() throws Exception {
-
         assertNotNull(userService.authUser("admin@email.com", "123"));
-
     }
 
     @Test
     public void createUserTest() {
+        User user = new User();
         user.setUsername("josh");
         user.setEmail("josh@email.com");
         user.setPassword("123");
         when(userServiceMock.registerUser(user)).thenReturn("User: " + user.getUsername() + " created successfully");
         assertThat(userServiceMock.registerUser(user)).contains("josh");
         verify(userServiceMock).registerUser(user);
-
     }
-
 
 }
