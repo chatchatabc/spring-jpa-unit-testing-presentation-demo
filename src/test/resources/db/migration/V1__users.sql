3d46ac8d507fa1332bdefce9aa0fe7b209eafd63
Libing Chen <libing.chen@gmail.com>
2023-01-31 11:20:11 +0800

    db: add salt for users

--- src/test/resources/db/migration/V1__users.sql
+++ src/test/resources/db/migration/V1__users.sql
@@ -3,6 +3,7 @@ CREATE TABLE users
     id SERIAL,
     username varchar(255) NOT NULL,
     password varchar(255) NOT NULL,
+    salt varchar(16) NOT NULL,
     email varchar(255) NOT NULL,
     CONSTRAINT pk_user PRIMARY KEY (id)
 );
